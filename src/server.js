d7415b8965c7b93eeb67c32e739e3be5cafab447
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2019-12-28 14:37:18 +0100

    Wrote tests that passed

--- src/server.js
+++ src/server.js
@@ -6,16 +6,25 @@ const app = express()
 config();
 
 app.use('/',(req, res)=>{
-    res.status.json('Welcome to API')
+    res.status(200).json('Welcome to API')
 });
 
 app.use(json());
 app.use(urlencoded({extended: false}));
 
-const PORT = 3200 || process.env.PORT;
+const PORT = process.env.PORT;
 
 app.listen(PORT, ()=>{
    console.log(`Server up and running on ${PORT}`);
 });
 
+
+// Invalid Routes
+app.all('*', (req, res) => res.status(404).json({
+    status: 'error',
+    code: 404,
+    message: 'Route unavailable on server.',
+  }));
+
+export const server = app;
 export default app;
