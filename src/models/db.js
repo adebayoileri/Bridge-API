064d716b357d1402a173ebce661b5249313322ad
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-01-02 22:31:04 +0100

    Conncted database

---
+++ src/models/db.js
@@ -0,0 +1,41 @@
+import { Client, Pool } from "pg";
+import { dotenv } from "dotenv";
+
+dotenv.config();
+
+const connectionString ={
+    user:process.env.DB_USERNAME,
+    password:process.env.DB_PASSWORD,
+    server:process.env.DB_SERVER,
+    port:process.env.DB_PORT,
+    database:process.env.DB_NAME
+}
+const pool = new Pool(connectionString)
+
+
+const UserTable = async () => {
+    const CreateTable = `CREATE TABLE IF NOT EXISTS
+      newusers(
+          id SERIAL PRIMARY KEY UNIQUE,
+          first_name VARCHAR(50) NOT NULL,
+          last_name VARCHAR(50) NOT NULL,
+          category VARCHAR(50) NOT NULL, 
+          email VARCHAR(50) NOT NULL,
+          password VARCHAR(20) NOT NULL
+          )`
+    try {
+      await pool.query(CreateTable)
+      console.log('user table created')
+  
+    }
+    catch (e) {
+      console.log(e)
+    }
+  
+  }
+
+pool.on('connect',()=>{ });
+
+UserTable();
+
+export default pool;
