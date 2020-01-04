ca44dd5a625c8ae93cd04efeabd21a2f75bdeb9a
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-01-04 10:19:09 +0100

    Ignored middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,17 +1,17 @@
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
-const secret = process.env.JWT_KEY;
- const checkAuth = (req, res, next) =>{
-try {
-    const decoded = req.headers.authorization.split(' ')[1]||req.body.token;
-    const decoded = jwt.verify(token, secret);
-    req.userData = decoded;
-    next();
-} catch (error) {
-    return res.status(401).json({
-        "message":"Not Authorized"
-    })
-}
- }
+// const secret = process.env.JWT_KEY;
+//  const checkAuth = (req, res, next) =>{
+// try {
+//     const decoded = req.headers.authorization.split(' ')[1]||req.body.token;
+//     const decoded = jwt.verify(token, secret);
+//     req.userData = decoded;
+//     next();
+// } catch (error) {
+//     return res.status(401).json({
+//         "message":"Not Authorized"
+//     })
+// }
+//  }
 
- export default checkAuth;
+//  export default checkAuth;
