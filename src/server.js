275dbb30d1f49deb72d4fd164eaa9eb03bc6dcff
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-02-26 15:40:17 +0100

    bug:(Fixed cannot import error module)

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+const express = require('express');
 import { json, urlencoded } from 'body-parser';
 import { config } from 'dotenv';
 import { logger } from 'morgan';
