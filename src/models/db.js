bba01108e3a42cd795fa62632935d035ce402086
Adebayo Ilerioluwa <46798106+adebayoileri@users.noreply.github.com>
2020-02-26 15:04:45 +0100

    Update db.js

--- src/models/db.js
+++ src/models/db.js
@@ -3,19 +3,22 @@ import { dotenv } from "dotenv";
 
 dotenv.config();
 
-if (process.env.NODE_ENV = 'development'){
-  let connectionString ={
-    user:process.env.DB_USERNAME,
-    password:process.env.DB_PASSWORD,
-    server:process.env.DB_SERVER,
-    port:process.env.DB_PORT,
-    database:process.env.DB_NAME
-}
-}else{
-  let connectionString = process.env.DB_URL;
-}
+// if (process.env.NODE_ENV = 'development'){
+//   let connectionString ={
+//     user:process.env.DB_USERNAME,
+//     password:process.env.DB_PASSWORD,
+//     server:process.env.DB_SERVER,
+//     port:process.env.DB_PORT,
+//     database:process.env.DB_NAME
+// }
+// }else{
+//   const connectionString = process.env.DB_URL;
+// }
 
-const pool = new Pool(connectionString)
+const pool = new Pool({
+    connectionString: process.env.DB_URL,
+    ssl:true,
+})
 
 
 const UserTable = async () => {
