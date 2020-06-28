6e226eccb5c07f0ec5f87a0f48197da317315869
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-06-28 21:19:38 +0100

    added signup and login routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,11 @@
+// imported middle ware to check token
+import checkToken from '../middlewares/auth';
 import {Router} from 'express';
+import Authentication from '../controllers/authController';
 
 const router = Router();
 
-router.post('/signup',(req, res)=>{
-    res.status(201).json({
-        "message":"Signup successful",
-    })
-});
+router.post('/signup',  Authentication.signUp);
+router.post('/login',  Authentication.login);
 
 export default router;
