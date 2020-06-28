2e2efba4b32c9aa18e216dcd34e59d217626d2ba
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-06-29 00:46:08 +0100

    connect express file upload and upload routes

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { json, urlencoded } from 'body-parser';
 import { config } from 'dotenv';
+import expressFileUpload from 'express-fileupload';
 import AuthRouters from './routes/authRoutes';
+import UploadRouter from './routes/uploadRoute';
 // import { logger } from 'morgan';
 // import taskRoutes from './routes/taskRoutes';
 // import authRoutes from './routes/authRoutes';
@@ -17,10 +19,14 @@ app.get('/',(req, res)=>{
 app.use(json());
 app.use(urlencoded({extended: false}));
 
+app.use(expressFileUpload({
+  useTempFiles: true
+}))
 // app.use('/api/v1',taskRoutes);
 // app.use('/api/v1/auth',authRoutes);
 
 app.use(AuthRouters)
+app.use(UploadRouter)
 
 const PORT = process.env.PORT || 3000;
 
