d74d4690d25848a561f9a68b81c4177625fde035
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-07-02 15:39:19 +0100

    modified updating of tasks login

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -161,48 +161,27 @@ class taskController {
       }})
   }
  /**
-  * @description - Only the owner of the task can update the task
-  * @param {object} req - /tasks/update/taskId?userId=1
+  * @description - Update a specific task
+  * @param {object} req - request object recieved
   * @param {object} res - response object sent
   *  @returns {object} - Task upadted
   */
 
   static async updateTask(req, res) {
     const { id } = req.params;
-    const {userid} = req.query;
-
-    const {
-      title,
-      bannerImg,
-      category,
-      description,
-      category_id,
-      location,
-      minbudget,
-      maxbudget,
-      startdate,
-      enddate,
-    } = req.body;
-    if (
-        !title ||
-        !bannerImg ||
-        !category ||
-        !description ||
-        !category_id ||
-        !minbudget ||
-        !maxbudget ||
-        !startdate ||
-        !enddate
-      ) {
-        return res.status(400).json({
-          message: 'All fields are required',
-        });
-      }else if(!req.query || !userid){
-        return res.status(400).json({
-          message: 'query userid is required',
-        });
-      }
-
+    // const {
+    //   title,
+    //   bannerImg,
+    //   category,
+    //   description,
+    //   category_id,
+    //   location,
+    //   minbudget,
+    //   maxbudget,
+    //   startdate,
+    //   enddate,
+    // } = req.body;
+ 
       jwt.verify(req.token, process.env.AUTHKEY, async (err, authorizedData)=> {
         if(err){
             return res.status(403).json({
@@ -212,17 +191,27 @@ class taskController {
             })
         }else{
           try {
-            const getSingleTaskQuery = `SELECT * FROM tasks WHERE id = $1 AND user_id=$2`;
-            const values = [parseInt(id), parseInt(userid)];
+            const getSingleTaskQuery = `SELECT * FROM tasks WHERE id = $1`;
+            const values = [parseInt(id)];
             const singleTask = await pool.query(getSingleTaskQuery, values);
-
+            const TaskToUpdate = singleTask.rows[0];
             if(!singleTask.rows[0]){
               return res.status(400).json({
                   status: "failed",
                   code: 400,
-                  message: "Task doesn\'t exists in db or task is not associated with this user"
+                  message: "Task doesn\'t exists in db"
               })
            }
+            const title = req.body.title || TaskToUpdate.title;
+            const bannerImg = req.body.category || TaskToUpdate.bannerImg;
+            const category = req.body.picture || TaskToUpdate.category;
+            const description = req.body.description || TaskToUpdate.description;
+            const category_id = req.body.category_id || TaskToUpdate.category_id;
+            const location = req.body.location || TaskToUpdate.location;
+            const minbudget = req.body.minbudget || TaskToUpdate.minbudget;
+            const maxbudget = req.body.maxbdget || TaskToUpdate.maxbudget;
+            const startdate = req.body.startdate || TaskToUpdate.startdate;
+            const enddate = req.body.enddate || TaskToUpdate.enddate;
             const updateTaskQuery = `UPDATE tasks SET title = $1, bannerImg = $2, category = $3, description = $4, category_id = $5,location = $6, minbudget = $7, maxbudget = $8, startdate = $9, enddate = $10, updatedat=CURRENT_TIMESTAMP WHERE id = $11 RETURNING id, title`;
             const valuesToUpdate = [
               title,
