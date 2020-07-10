8ef0c7b95354cd2673cd9408a729789f4add050c
Anani oluwatobiloba <ananioluwatobiloba2000@gmail.com>
2020-07-10 01:27:16 +0100

    modified validation for edit profile

--- src/middlewares/userValidation.js
+++ src/middlewares/userValidation.js
@@ -3,9 +3,9 @@ import Joi from '@hapi/joi';
 
 const validateEditProfile = profile => {
     const JoiSchema = Joi.object({
-      first_name: Joi.string().min(3).max(30).required(),
-      last_name: Joi.string().min(3).max(30).required(),
-      phonenumber: Joi.string().min(10).max(15).required()
+      first_name: Joi.string().min(3).max(30),
+      last_name: Joi.string().min(3).max(30),
+      phonenumber: Joi.string().min(10).max(15)
     }).options({
       abortEarly: false
     });
